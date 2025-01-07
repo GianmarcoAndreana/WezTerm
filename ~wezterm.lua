f4b321c633ca4bfc148edd03570b25cdd87d3d14
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2025-01-07 12:35:11 +0100

    Update configuration to support multiplex and neovim

--- ~wezterm.lua
+++ ~wezterm.lua
@@ -1,103 +1,90 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 local mux = wezterm.mux
-
--- FullScreen on startup
+local action = wezterm.action
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
+	local _, _, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
-
--- Configure the font
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
+local function is_vim(pane)
+	local process_info = pane:get_foreground_process_info()
+	local process_name = process_info and process_info.name
+  
+	return process_name == "nvim" or process_name == "vim"
+  end
+-- local function is_vim(pane)
+-- 	-- This gsub is equivalent to POSIX basename(3)
+-- 	-- Given "/foo/bar" returns "bar"
+-- 	-- Given "c:\\foo\\bar" returns "bar"
+-- 	local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
+-- 	return process_name == "nvim" or process_name == "vim"
+-- end
 
 -- Spawn a PowerShell in login mode
 config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe" }
 
--- Never prompt exit confirmation windows
-config.window_close_confirmation = "NeverPrompt"
-
--- Remove the window bar above keeping it still resizable
-config.window_decorations = "RESIZE"
-
--- Font size
-config.font_size = 16
-
--- Size of window
-config.window_frame = {
-	font_size = 13.0,
-}
-
--- Window padding effect
-config.window_padding = {
-	left = 3,
-	right = 3,
-	top = 0,
-	bottom = 0,
-}
-
--- tab bar
-config.hide_tab_bar_if_only_one_tab = false
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-config.tab_and_split_indices_are_zero_based = true
-
--- Use true colors
-config.term = "xterm-256color"
-
--- tmux config from here: https://dev.to/lovelindhoni/make-wezterm-mimic-tmux-5893
-
 local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
 	h = "Left",
 	j = "Down",
 	k = "Up",
 	l = "Right",
-}
+  }
 
-local function split_nav(key)
+config.font = wezterm.font("JetBrains Mono")
+
+local function split_nav(resize_or_move, key)
 	return {
-		key = key,
-		mods = "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if pane:Get_users_vars().IS_NVIM == "true" then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = { key = key, mods = "CTRL" },
-				}, pane)
-			else
-				win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-			end
-		end),
+	  key = key,
+	  mods = resize_or_move == "resize" and "META" or "CTRL",
+	  action = wezterm.action_callback(function(win, pane)
+		if is_vim(pane) then
+		  -- pass the keys through to vim/nvim
+		  win:perform_action({
+			SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+		  }, pane)
+		else
+		  if resize_or_move == "resize" then
+			win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+		  else
+			win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+		  end
+		end
+	  end),
 	}
-end
+  end
 
-config.font_size = 16
-
+config.font_size = 18
 config.window_decorations = "RESIZE"
-
+config.window_frame = {
+	font_size = 13.0,
+}
+-- config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.show_new_tab_button_in_tab_bar = false
 config.window_padding = {
-	top = 10,
-	bottom = 10,
-	left = 10,
-	right = 10,
+	bottom = 0,
+	left = 0,
+	right = 0,
 }
 
--- tmux
--- Leader
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
 
-local action = wezterm.action
+-- config.window_background_opacity = 0.8
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.warn_about_missing_glyphs = false
 
 config.keys = {
-	--Splitting Panes
+
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
 	{
 		key = "\\",
 		mods = "LEADER",
@@ -108,45 +95,16 @@ config.keys = {
 		mods = "LEADER",
 		action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
-	split_nav("h"),
-	split_nav("j"),
-	split_nav("k"),
-	split_nav("l"),
-	-- Adjusting pane size
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = action.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		key = "l",
-		mods = "CTRL|SHIFT",
-		action = action.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		key = "j",
-		mods = "CTRL|SHIFT",
-		action = action.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		key = "k",
-		mods = "CTRL|SHIFT",
-		action = action.AdjustPaneSize({ "Up", 5 }),
-	},
-
 	{
 		key = "m",
 		mods = "LEADER",
 		action = action.TogglePaneZoomState,
 	},
-	-- Setup VI Mode
-	{ key = "[", mods = "LEADER", action = action.ActivateCopyMode },
-
-	-- Tab configuration
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
 	{
 		key = "c",
 		mods = "LEADER",
-		action = action.SpawnTab("CurrentPaneDomain"),
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
 	},
 
 	{
@@ -159,16 +117,61 @@ config.keys = {
 		mods = "LEADER",
 		action = action.ActivateTabRelative(1),
 	},
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+	{
+		key = "9",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|TABS|WORKSPACES" }),
+	},
+	{
+		key = "W",
+		mods = "CTRL|SHIFT",
+		action = action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
 }
-
--- loop to navigate tabs
-for i = 0, 9 do
+for i = 1, 9 do
 	table.insert(config.keys, {
 		key = tostring(i),
 		mods = "LEADER",
-		action = action.ActivateTab(i),
+		action = action.ActivateTab(i - 1),
 	})
 end
+
+config.status_update_interval = 10000
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
@@ -186,11 +189,12 @@ wezterm.on("update-right-status", function(window, _)
 	end -- arrow color based on if tab is first pane
 
 	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
+		{ Background = { Color = "#cba6f7" } },
 		{ Text = prefix },
 		ARROW_FOREGROUND,
 		{ Text = SOLID_LEFT_ARROW },
 	}))
 end)
+
 
 return config
